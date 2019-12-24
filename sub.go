4a1ae5f55bd6f38b92caefd2bbee3f440f562269
nghiatc <congnghia0609@gmail.com>
2019-12-24 16:39:31 +0700

    update code

--- sub.go
+++ sub.go
@@ -7,17 +7,53 @@ package main
 
 import (
 	"fmt"
+	"github.com/congnghia0609/ntc-gconf/nconf"
 	"log"
+	"ntc-gnats/nsub"
+	"os"
+	"os/signal"
+	"path/filepath"
 	"runtime"
+	"strconv"
 	"time"
 	"github.com/nats-io/nats.go"
 )
+
+func InitNConf5() {
+	_, b, _, _ := runtime.Caller(0)
+	wdir := filepath.Dir(b)
+	fmt.Println("wdir:", wdir)
+	nconf.Init(wdir)
+}
 
 /**
  * cd ~/go-projects/src/ntc-gnats
  * go run sub.go
  */
 func main() {
+	// Init NConf
+	InitNConf5()
+
+	//// InitSub
+	nsub.InitSubConf("chat")
+	// Init PoolNSubscriber
+	var poolnsub nsub.PoolNSubscriber
+	for i:=0; i<2; i++ {
+		ns := nsub.NSubscriber{strconv.Itoa(i), "msg.test", nil, nil}
+		poolnsub.AddNSub(ns)
+	}
+	poolnsub.RunPoolNSub()
+
+	// Hang thread Main.
+	s := make(chan os.Signal, 1)
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C) SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(s, os.Interrupt)
+	// Block until we receive our signal.
+	<-s
+	log.Println("################# End Main #################")
+}
+
+func testSub() {
 	// DefaultURL: nats://127.0.0.1:4222
 	var urls = nats.DefaultURL
 	var showTime = true
